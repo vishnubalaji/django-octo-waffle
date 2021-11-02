b6be25c103fce2664aa3ba96597f190a9431fe74
Vishnu Balaji <v2b2y2k@gmail.com>
2021-11-02 16:11:29 +0530

    Login and logout completed

--- mysite/users/views.py
+++ mysite/users/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from .forms import UserRegisterForm
+from django.contrib.auth.decorators import login_required
 
 def register(request):
     if(request.method == 'POST'):
@@ -9,7 +10,11 @@ def register(request):
             form.save()
             username = form.cleaned_data.get('username')
             messages.success(request, f'Account created for {username}')
-            return redirect('polls-home')
+            return redirect('login')
     else:
         form = UserRegisterForm()
     return render(request, 'users/register.html', {'form':form})
+
+@login_required
+def profile(request):
+    return render(request, 'users/profile.html')
